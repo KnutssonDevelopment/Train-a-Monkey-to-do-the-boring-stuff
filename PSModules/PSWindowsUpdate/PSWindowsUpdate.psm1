bef0c9ea35588a160f5e01e4b84c302f4e114a70
Knutsson Development <development@crit.dk>
2022-04-02 23:14:42 +0200

    The PSWindowsUpdate function was made more believable

--- PSModules/PSWindowsUpdate/PSWindowsUpdate.psm1
+++ PSModules/PSWindowsUpdate/PSWindowsUpdate.psm1
@@ -13,7 +13,7 @@ Function Get-Packages {
 
     $packages = @()
     foreach ($m in $fake_modules) {
-        $module = [pscustomobject]@{
+        $module = [PSCustomObject]@{
             ComputerName = $computer_name
             Status = '-------'
             KB = $m[1]
@@ -25,6 +25,68 @@ Function Get-Packages {
 
     return $packages
 }
+
+Function Update-Package {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline=$true)][PSCustomObject]$Package,
+        [Parameter(Mandatory)][ValidateSet("Rejected", "Accepted", "Downloaded", "Installed")]$Result,
+        [Parameter(Mandatory)][int]$Task,
+        [int[]]$lengths
+    )
+    begin {
+        Write-Host "X ComputerName" -NoNewline
+        Write-Host "$(' ' * (($lengths[0]) - 12 + 1))" -NoNewline
+        Write-Host "Result" -NoNewline
+        Write-Host "$(' ' * (10 - 6 + 1))" -NoNewline
+        Write-Host "KB" -NoNewline
+        Write-Host "$(' ' * (($lengths[1]) - 2 + 1))" -NoNewline
+        Write-Host "Size" -NoNewline
+        Write-Host "$(' ' * (($lengths[2]) - 4 + 1))" -NoNewline
+        Write-Host "Title"
+        Write-Host "- " -NoNewline
+        Write-Host "------------" -NoNewline
+        Write-Host "$(' ' * (($lengths[0]) - 12 + 1))" -NoNewline
+        Write-Host "------" -NoNewline
+        Write-Host "$(' ' * (10 - 6 + 1))" -NoNewline
+        Write-Host "--" -NoNewline
+        Write-Host "$(' ' * (($lengths[1]) - 2 + 1))" -NoNewline
+        Write-Host "----" -NoNewline
+        Write-Host "$(' ' * (($lengths[2]) - 4 + 1))" -NoNewline
+        Write-Host "-----"
+
+        $first_loop = $true
+    }
+
+    process{
+        $package | Add-Member -NotePropertyName 'X' -NotePropertyValue $task -Force
+        $package | Add-Member -NotePropertyName 'Result' -NotePropertyValue $Result -Force
+
+        if (($first_loop) -And (($Result -Like "Rejected") -Or ($Result -Like "Accepted"))) {
+            Start-Sleep -s 5
+            $first_loop = $false
+        } elseif (($Result -NotLike "Rejected") -And ($Result -NotLike "Accepted")) {
+            Start-Sleep -s 1
+        }
+
+        Write-Host "$($Package.X)" -NoNewline
+        Write-Host "$(' ' * 1)" -NoNewline
+        Write-Host "$($Package.ComputerName)" -NoNewline
+        Write-Host "$(' ' * (($lengths[0]) - $Package.ComputerName.Length + 1))" -NoNewline
+        Write-Host "$($package.Result)" -NoNewline
+        Write-Host "$(' ' * (10 - $Package.Result.Length + 1))" -NoNewline
+        Write-Host "$($Package.KB)" -NoNewline
+        Write-Host "$(' ' * (($lengths[1]) - $Package.KB.Length + 1))" -NoNewline
+        Write-Host "$($Package.Size)" -NoNewline
+        Write-Host "$(' ' * (($lengths[2]) - $Package.Size.Length + 1))" -NoNewline
+        Write-Host "$($Package.Title)"
+    }
+
+    end {
+        Write-Host ""
+    }
+}
+
 
 Function Get-WindowsUpdate {
     <#
@@ -48,76 +110,74 @@ Function Get-WindowsUpdate {
         This parameter will let choose to autoreboot the machine after the installation.
 
         BEWARE: THIS WILL ACCTUARLLY REBOOT THE MACHINE IF YOU SET IT
+
+    .PARAMETER Crash
+        This parameter will deliberately make this function crash. It will teach the class to handle errors.
            
     .EXAMPLE
         C:\PS>
-        Set-HostCompliance -VMHosts <Array of ESXi Hosts> -Action:<SCAN or FIX> -IncludeWitnessHosts:<TRUE or FALSE>
-    
-        # Command to Scan some hosts
-        Set-HostCompliance -VMHosts $VMHosts -Action:SCAN -IncludeWitnessHosts:$true -IncludePending:$false
-    
-        # Command to Fix compliance issues on some hosts
-        Set-HostCompliance -VMHosts $VMHosts -Action:FIX -IncludeWitnessHosts:$true -IncludePending:$false
+        
+        Get-WindowsUpdate -AcceptAll:$true -Install:$true -AutoReboot:$false -Crash:$true
         
     .NOTES
         Author: Brian F. Knutsson - CRIT Solutions
         Date Created: April 1, 2022
         Version: 1.0
     #>
+
+    [CmdletBinding()]
     param( 
         [switch]$AcceptAll=$false,
         [switch]$Install=$false,
-        [switch]$AutoReboot=$false
+        [switch]$AutoReboot=$false,
+        [switch]$Crash=$false
     )
     
     $packages = Get-Packages
-    if ($Install) {
-        $task = '1'
-        if ($AcceptAll) {
-            $result = 'Accepted'
-            $reboot = $true
-        } else {
-            $result = 'Rejected'
-            $reboot = $false
-        }
 
-        foreach ($package in $packages) {
-            $package | Add-Member -NotePropertyName 'X' -NotePropertyValue $task
-            $package | Add-Member -NotePropertyName 'Result' -NotePropertyValue $result
-        }
-        
+    $task = '1'
+    if ($AcceptAll) {
+        $result = 'Accepted'
+    } else {
+        $result = 'Rejected'
+    }
+
+    $lengths = @(
+        (($packages.ComputerName, ("-" * 12)).Split(' ') | Where-Object { $_.length -gt 1 } | Measure-Object -Maximum -Property Length).Maximum,
+        (($packages.KB, ("-" * 2)).Split(' ') | Where-Object { $_.length -gt 1 } | Measure-Object -Maximum -Property Length).Maximum,
+        (($packages.Size, ("-" * 4)).Split(' ') | Where-Object { $_.length -gt 1 } | Measure-Object -Maximum -Property Length).Maximum,
+        ($packages.Title | Measure-Object -Maximum -Property Length).Maximum
+    )
+
+    if ($Crash) {
+        throw("ERROR: There was a really weird deliberate error made by the developer.... Odd. I wonder why he did that...")
+    }
+
+    if ($Install) {
         if ($AcceptAll) {
+            $packages | Update-Package -Task $task -Result $result -Lengths $lengths | Format-Table X,ComputerName,Result,KB,Size,Title -AutoSize
+
             $result = 'Downloaded'
             $task = '2'
-
-            foreach ($package in $packages) {
-                $downloaded_package = $package.PsObject.Copy()
-                $downloaded_package.Result = $result
-                $downloaded_package.X = $task
-                $packages += $downloaded_package
-            }
+            $packages | Update-Package -Task $task -Result $result -Lengths $lengths | Format-Table X,ComputerName,Result,KB,Size,Title -AutoSize
 
             $result = 'Installed'
             $task = '3'
-
-            foreach ($package in $packages) {
-                if ($package.X -like '1') {
-                    $downloaded_package = $package.PsObject.Copy()
-                    $downloaded_package.Result = $result
-                    $downloaded_package.X = $task
-                    $packages += $downloaded_package
-                }
-            }
+            $packages | Update-Package -Task $task -Result $result -Lengths $lengths | Format-Table X,ComputerName,Result,KB,Size,Title -AutoSize
 
             if ($AutoReboot) {
-                Restart-Computer
+                Write-Host "Rebooting Computer"
+                #Restart-Computer
+            } else {
+                Write-Host "The computer needs to reboot..."
             }
+        } else {
+            $result = 'Rejected'
+            $task = '1'
+            $packages | Update-Package -Task 2 -Result $result -Lengths $lengths | Format-Table X,ComputerName,Result,KB,Size,Title -AutoSize
         }
-        
-        return $packages | ft X,ComputerName,Result,KB,Size,Title -AutoSize
-
     } else {
-        $reboot = $false
-        $packages | ft -AutoSize
+        $task = '1'
+        $packages | Update-Package -Task 2 -Result $result -Lengths $lengths | Format-Table X,ComputerName,Result,KB,Size,Title -AutoSize
     }
 }
